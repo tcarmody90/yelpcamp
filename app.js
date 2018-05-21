1983ba165904285f3546437f4ac0ee6e50270b50
Thomas Carmody <tcarmody90@yahoo.com>
2018-05-21 22:06:37 +0000

    add mLab mongoDB hosting

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp_v12");
+//mongoose.connect("mongodb://localhost/yelp_camp_v12");
+mongoose.connect("mongodb://tcarmody:raider1@ds231070.mlab.com:31070/yelpcampdb");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
