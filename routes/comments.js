671a83d537c538b1831ed6ce62bd96de13860575
Thomas Carmody <tcarmody90@yahoo.com>
2018-05-21 21:29:52 +0000

    initial deploy commit

---
+++ routes/comments.js
@@ -0,0 +1,88 @@
+var express     = require("express");
+var router      = express.Router({mergeParams: true});
+var Campground  = require("../models/campground");
+var Comment     = require("../models/comment");
+var middleware  = require("../middleware");
+
+// COMMENT NEW
+router.get("/new", middleware.isLoggedIn, function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+       if(err){
+            req.flash("error", "Something went wrong");
+           console.log(err);
+       } else {
+           res.render("comments/new", {campground: campground});
+       }
+    });
+});
+
+// COMMENT CREATE
+router.post("/", middleware.isLoggedIn, function(req, res){
+   //look up campground using ID
+   console.log(req.params.id);
+   Campground.findById(req.params.id, function(err, campground){
+       if(err){
+           console.log(err);
+       } else {
+           Comment.create(req.body.comment, function(err, comment){
+               if(err){
+                   console.log(err);
+               } else {
+                   //add username and id to comment
+                   comment.author.id = req.user._id;
+                   comment.author.username = req.user.username;
+                   //save comment
+                   comment.save();
+                   campground.comments.push(comment);
+                   campground.save();
+                   console.log(comment);
+                   req.flash("success", "Successfully Added Comment");
+                   res.redirect('/campgrounds/' + campground._id);
+               }
+           });
+       }
+   });
+});
+
+// COMMENT EDIT ROUTE
+router.get("/:comment_id/edit", middleware.checkCommentOwnership, function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err || !foundCampground){
+            req.flash("error", "No Campground Found");
+            return res.redirect("back");
+        }
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+            if(err){
+                res.redirect("back");
+            } else {
+                res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+            }
+        });
+    });
+});
+
+// COMMENTS UPDATE
+router.put("/:comment_id", middleware.checkCommentOwnership, function(req, res){
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+       if(err){
+           res.redirect("back");
+       }  else {
+            res.redirect("/campgrounds/" + req.params.id);
+       }
+    });
+});
+
+// COMMENTS DESTROY ROUTE
+router.delete("/:comment_id", middleware.checkCommentOwnership, function(req, res){
+    //findByIdAndRemove
+    Comment.findByIdAndRemove(req.params.comment_id, function(err){
+        if(err){
+            res.redirect("back");
+        } else {
+            req.flash("error", "Comment Deleted");
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
+module.exports = router;
