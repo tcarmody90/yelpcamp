c373321ccb356abbbc68730c85d81d3f10653d20
Thomas Carmody <tcarmody90@yahoo.com>
2018-05-22 20:38:46 +0000

    add environmental variable for db url

--- app.js
+++ app.js
@@ -16,9 +16,8 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
 
-mongoose.connect(process.env.DATABASEURL);
-//mongoose.connect("mongodb://localhost/yelp_camp_v12");
-//mongoose.connect("mongodb://tcarmody:raider1@ds231070.mlab.com:31070/yelpcampdb");
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v12";
+mongoose.connect(url);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
